1c9e8abc0eb65cf46902122f7d97ba410aeb1732
JR <rolyordonezleon@gmail.com>
2023-02-25 15:25:01 -0500

    create of model owner in app owner

--- app_pokemon/owner/models.py
+++ app_pokemon/owner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Owner(models.Model):
+   nombre = models.CharField(max_length=40)
+   edad = models.IntegerField()
+   pais = models.CharField(max_length=27, default= '')
+   dni = models.CharField(max_length=8, default='00000000')
+   vigente = models.BooleanField(default=True)
