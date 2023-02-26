20b75783d219818a4c537c2cd60582d54f165563
JR <rolyordonezleon@gmail.com>
2023-02-26 08:06:01 -0500

    labels in templates django

--- app_pokemon/app_pokemon/settings.py
+++ app_pokemon/app_pokemon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.static',
     'owner.apps.OwnerConfig',
     'pokemon.apps.PokemonConfig'
 ]
@@ -122,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- app_pokemon/owner/views.py
+++ app_pokemon/owner/views.py
@@ -2,6 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def list_owner(request):
-    print("Probando vista!! ")
-    name = "CERSEU"
-    return HttpResponse(name)
+
+    data_context = {
+        'nombre': 'Jesús de La Cruz',
+        'edad': 29,
+        'pais_nacimiento':'Peru',
+        'dni': '951456123',
+        'vigente': False
+    }
+
+    data_context = [
+        {
+            'nombre': 'Jesús de La Cruz',
+            'edad': 26,
+            'pais_nacimiento': 'Peru',
+            'dni': '451456653',
+            'vigente': True
+        },
+        {
+            'nombre': 'Eduardo Gutierrez',
+            'edad': 28,
+            'pais_nacimiento': 'Brasil',
+            'dni': '4651456123',
+            'vigente': True
+        },
+        {
+            'nombre': 'Maria Luiza',
+            'edad': 35,
+            'pais_nacimiento': 'México',
+            'dni': '4123456123',
+            'vigente': True
+        }
+    ]
+
+    return render(request, 'owner/owner_list.html', context={'data':data_context})
